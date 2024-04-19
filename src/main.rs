5cf6d26781a97e30e158527e512d510f27df5308
nomissbowling <nomissbowling@yahoo.co.jp>
2024-04-19 09:13:43 +0900

    version 0.9.1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/ode-rs-0000/0.9.0")]
+#![doc(html_root_url = "https://docs.rs/ode-rs-0000/0.9.1")]
 /*
   cc-rs https://crates.io/crates/cc
   bindgen https://crates.io/crates/bindgen
